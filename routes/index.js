a40855e790f030deb3446a7a08b2ce329950c3f6
Kurobane Yan <yan.kurobane@gmail.com>
2017-06-18 12:23:33 +0300

    change functions to an arrow functions in routers

--- routes/index.js
+++ routes/index.js
@@ -3,31 +3,31 @@ let router = express.Router()
 
 const db = require('../db/index')
 
-router.get('/', function (req, res, next) {
+router.get('/', (req, res, next) => {
   db.getAll()
   .then(cars => res.json(cars))
   .catch(error => res.json({error: error}))
 })
 
-router.get('/catalog', function (req, res, next) {
+router.get('/catalog', (req, res, next) => {
   db.getCatalog()
   .then(c => res.json(c))
   .catch(error => res.json({error: error}))
 })
 
-router.post('/create', function (req, res, next) {
+router.post('/create', (req, res, next) => {
   db.create(req.body)
   .then(c => res.json(c))
   .catch(error => res.json({error: error}))
 })
 
-router.get('/read/:id', function (req, res, next) {
+router.get('/read/:id', (req, res, next) => {
   db.read(req.params.id)
   .then(c => res.json(c))
   .catch(error => res.json({error: error}))
 })
 
-router.put('/update/:id', function (req, res, next) {
+router.put('/update/:id', (req, res, next) => {
   db.update(req.params.id, req.body)
   .then(c => res.json(c))
   .catch(error => res.json({error: error}))
