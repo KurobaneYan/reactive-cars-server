c3a709bd2bde648a0bde4cdbc62e3c451c742f08
Kurobane Yan <yan.kurobane@gmail.com>
2017-06-18 12:58:17 +0300

    refactor route file

--- routes/index.js
+++ routes/index.js
@@ -3,46 +3,38 @@ let router = express.Router()
 
 const db = require('../db/index')
 
+const show = (promise, res) => {
+  promise
+  .then(data => res.json(data))
+  .catch(error => res.json({ error: error }))
+}
+
 router.get('/', (req, res, next) => {
-  db.getAll()
-  .then(cars => res.json(cars))
-  .catch(error => res.json({error: error}))
+  show(db.getAll(), res)
 })
 
 router.get('/catalog', (req, res, next) => {
-  db.getCatalog()
-  .then(c => res.json(c))
-  .catch(error => res.json({error: error}))
+  show(db.getCatalog(), res)
 })
 
 router.get('/show/:id', (req, res, next) => {
-  db.show(req.params.id)
-  .then(c => res.json(c))
-  .catch(error => res.json({error: error}))
+  show(db.show(req.params.id), res)
 })
 
 router.post('/create', (req, res, next) => {
-  db.create(req.body)
-  .then(c => res.json(c))
-  .catch(error => res.json({error: error}))
+  show(db.create(req.body), res)
 })
 
 router.get('/read/:id', (req, res, next) => {
-  db.read(req.params.id)
-  .then(c => res.json(c))
-  .catch(error => res.json({error: error}))
+  show(db.read(req.params.id), res)
 })
 
 router.put('/update/:id', (req, res, next) => {
-  db.update(req.params.id, req.body)
-  .then(c => res.json(c))
-  .catch(error => res.json({error: error}))
+  show(db.update(req.params.id, req.body), res)
 })
 
 router.delete('/delete/:id', (req, res, next) => {
-  db.delete(req.params.id)
-  .then(c => res.json(c))
-  .catch(error => res.json({error: error}))
+  show(db.delete(req.params.id), res)
 })
 
 module.exports = router
